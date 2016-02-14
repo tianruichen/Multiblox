8d0690a1e9bc20c746d716e720103b1ffe85921b
tianruichen <tianruichen@users.noreply.github.com>
2016-02-14 05:02:18 -0800

    started drawing stuff

--- public/js/game.js
+++ public/js/game.js
@@ -1,19 +1,24 @@
 var canvas,
 	ctx,
-	player;
+	player,
+	ratio = 1,
+	height = 30,
+	width = 30,
+	gameWidth = 900,
+	gameHeight = 900;
 
 function init() {
 	canvas = document.getElementById("gameCanvas");
 	ctx = canvas.getContext("2d");
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+	canvas.width = 900;
+	canvas.height = 900;
 	socket = io.connect('http://localhost:8000/');
 	setEventHandlers();
 }
 
 function setEventHandlers() {
 	window.addEventListener("keydown", onKeydown, false);
-	window.addEventListener("resize", onResize, false);
+	//window.addEventListener("resize", onResize, false);
 	socket.on("connect", onSocketConnected);
 	socket.on("disconnect", onSocketDisconnect);
 	socket.on('getgame', updateGameState)
@@ -33,9 +38,19 @@ function onKeydown(e) {
 	if (d) socket.emit("keypress", {key: d});
 }
 
-function onResize(e) {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
+function onResize() {
+	if (window.innerWidth / gameWidth * gameHeight > window.innerHeight) {
+		var hratio = window.innerHeight / gameHeight;
+		ctx.scale(hratio, hratio);
+		console.log(hratio);
+	}
+	else {
+		var wratio = window.innerWidth / gameWidth;
+		ctx.scale(wratio, wratio);
+		console.log(wratio);
+	}
+	console.log(canvas.width);
+	console.log(canvas.height);
 };
 
 function onSocketConnected() {
@@ -49,11 +64,32 @@ function onSocketDisconnect() {
 function updateGameState(){
 }
 
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       || 
+          window.webkitRequestAnimationFrame || 
+          window.mozRequestAnimationFrame    || 
+          window.oRequestAnimationFrame      || 
+          window.msRequestAnimationFrame     || 
+          function(/* function */ callback, /* DOMElement */ element){
+            window.setTimeout(callback, 1000 / 60);
+          };
+})();
 
 function animate() {
 	draw();
+	window.requestAnimFrame(animate);
 }
 
 function draw() {
+	console.log("testing")
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.fillStyle = '#FF0000';
+	ctx.fillRect(0, 150, 125, 125);
+	ctx.fillRect(150, 100, 600, 600);
+	ctx.fillRect(800, 50, 100, 700);
+	
+}
+
+function drawBlock(x, y, marginX, marginY) {
+	ctx.fillRect(x - 10 + marginX, y - 10 + marginY, x + 10 + marginX, y + 10 + marginY);
 }
