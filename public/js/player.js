a9f1c65247e6f722fcf9981634a5e2797c9711e9
Matthew Wu <matt.j.wu13@gmail.com>
2016-02-14 15:32:06 -0800

    added spawnRow and spawnCol to player, player takes in conveyor and hold in addition to action and grid and acts accordingly

--- public/js/player.js
+++ public/js/player.js
@@ -5,18 +5,20 @@ Game Player Class
 var iNeedTheHeldPiece = 73;
 var iNeedTheQueuePiece = 20;
 
-var Player = function(username, id) {
+var Player = function(username, id, spawnRow, spawnCol) {
 	this.username = username;
 	this.id = id;
 	this.piece = null;
 	this.canHold = true;
+	this.spawnRow = spawnRow;
+	this.spawnCol = spawnCol;
 }
 
-Player.update = function(grid, action, conveyor) {
+Player.update = function(grid, action, conveyor, hold) {
 	if (action == "hold") {
 		if (this.canHold) {
 			this.canHold = false;
-			return iNeedTheHeldPiece
+			this.piece = new Piece(grid, hold.getPiece(this.piece.blockType), this.spawnRow, this.spawnCol);
 		}
 	}
 	else {
@@ -24,13 +26,18 @@ Player.update = function(grid, action, conveyor) {
 		this.canHold = true;
 		result = this.piece.update(grid, action);
 		if (result) {
-			this.piece = conveyor.getPiece();
+			this.piece = new Piece(grid, conveyor.getPiece(), this.spawnRow, this.spawnCol);
 		}
 	}
 }
 
 Player.getSquares = function() {
 	return this.piece.getSquares();
+}
+
+Player.setSpawn = function(newRow, newCol) {
+	this.spawnRow = newRow;
+	this.spawnCol = newCol;
 }
 
 if (typeof module !== "undefined") module.exports = Player;
